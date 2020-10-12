d5aa8be522649a9a9c0afaa3e53dc527e52730c4
Parth Parakh <parthparakh95@gmail.com>
2020-10-12 21:28:10 +0100

    Update No of Islands.cpp

--- No of Islands.cpp
+++ No of Islands.cpp
@@ -41,3 +41,16 @@ private:
         return;
     }
 };
+
+
+// 
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> s;
+        for(auto n:nums)
+        {
+            if(s.find(n)!=s.end()) return true;
+            s.insert(n);
+        }
+        return false;
+    }
