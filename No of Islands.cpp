677f567026492b56cdea2bcd59dabc5823594189
Parth Parakh <parthparakh95@gmail.com>
2020-10-18 18:16:07 +0100

    Update No of Islands.cpp

--- No of Islands.cpp
+++ No of Islands.cpp
@@ -66,3 +66,13 @@ public:
         return root;
     }
 };
+
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(!p && !q) return true;
+        if(!p || !q) return false;
+        return (p->val==q->val && isSameTree(p->left,q->left) && isSameTree(p->right,q->right) );
+    }
+};
