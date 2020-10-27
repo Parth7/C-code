697cee69cae0f24104e712e5b20cf249e6e42bf5
Parth Parakh <parthparakh95@gmail.com>
2020-10-27 06:44:42 +0000

    Add files via upload

---
+++ unique_paths.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        if(obstacleGrid.size()==0) return 0;
+        int n = obstacleGrid.size();
+        int m = obstacleGrid[0].size();
+        if(obstacleGrid[0][0]==1 || obstacleGrid[n-1][m-1]==1) return 0;
+        vector<vector<int>> dp(n,vector<int>(m,0));
+        for(int i=0;i<n;i++)
+        {
+            if(obstacleGrid[i][0]==0) dp[i][0] = 1;
+            else break;
+        }
+        
+        for(int j=0;j<m;j++)
+        {
+            if(obstacleGrid[0][j]==0) dp[0][j] = 1;
+            else break;
+        }
+        
+        for(int i=1;i<n;i++)
+        {
+            for(int j=1;j<m;j++)
+            {
+                if(obstacleGrid[i-1][j-1]==0)
+                {
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                }
+            }
+        }
+        return dp[n-1][m-1];
+    }
+};
